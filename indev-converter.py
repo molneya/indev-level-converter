632a18a6570b28808861874ac757c7ef7243d5f5
molneya <62799417+molneya@users.noreply.github.com>
2020-12-09 20:49:36 +0800

    Add files via upload

--- indev-converter.py
+++ indev-converter.py
@@ -3,50 +3,66 @@ from nbtlib import *
 import numpy
 import os
 
-#Make world folder
-try: os.mkdir('World1')
+#Find world
+worlds = [f for f in os.listdir() if f.endswith('.mclevel')]
+try: nbt_file = nbtlib.load(worlds[0])
 except:
-    print('World folder already exists, please delete or rename to proceed')
+    print("No worlds found! Check that the file extension is .mclevel and that you have placed something inside the folder.")
     input()
     exit()
 
-#load world and datas
-worlds = [f for f in os.listdir() if f.endswith('.mclevel')]
-nbt_file = nbtlib.load(worlds[0])
+#Make world folder
+try: os.mkdir('World1')
+except:
+    print('World folder already exists, please delete or rename to proceed.')
+    input()
+    exit()
 
+#Load root tags
 About = nbt_file.root['About']
+Environment = nbt_file.root['Environment']
 Map = nbt_file.root['Map']
 Entities = nbt_file.root['Entities'] #Does this need to be implemented? early infdev doesnt save entities anyway.
 TileEntities = nbt_file.root['TileEntities']
 
+print("getting inventory data...")
 #Get data
 for i in Entities:
     if "Inventory" in i:
         Inventory = i["Inventory"]
-        Motion0 = float(i["Motion"][0])
-        Motion1 = float(i["Motion"][1])
-        Motion2 = float(i["Motion"][2])
-        Pos0 = float(i["Pos"][0])
-        Pos1 = float(i["Pos"][1]+32)
-        Pos2 = float(i["Pos"][2])
+        Motion0 = Double(i["Motion"][0])
+        Motion1 = Double(i["Motion"][1])
+        Motion2 = Double(i["Motion"][2])
+        Pos0 = Double(i["Pos"][0])
+        Pos1 = Double(i["Pos"][1]+32)
+        Pos2 = Double(i["Pos"][2])
         Rotation = i["Rotation"]
         Air = i["Air"]
+        try: AttackTime = i["AttackTime"]
+        except: AttackTime = Short(0)
+        try: DeathTime = i["DeathTime"]
+        except: DeathTime = Short(0)
         FallDistance = i["FallDistance"]
         Fire = i["Fire"]
-        id = i["id"]
+        try: Health = i["Health"]
+        except: Health = Short(10)
+        try: HurtTime = i["HurtTime"]
+        except: HurtTime = Short(0)
+        OnGround = Byte(0)
         Score = i["Score"]
         
-SpawnX = int(Map["Spawn"][0])
-SpawnY = int(Map["Spawn"][1]+32)
-SpawnZ = int(Map["Spawn"][2])
-
+SpawnX = Int(Map["Spawn"][0])
+SpawnY = Int(Map["Spawn"][1]+32)
+SpawnZ = Int(Map["Spawn"][2])
 LastPlayed = About["CreatedOn"]
+try: Time = Long(Environment["TimeOfDay"])
+except: Time = Long(0)
 
 Blocks = Map['Blocks']
 Data = Map['Data']
 Zeroes = [Byte(0)] * 32768
 
-print("getting tile entities...")
+print("getting tile entity data...")
 #Get tile entities
 Tiles = []
 for i in TileEntities:
@@ -57,40 +73,7 @@ for i in TileEntities:
     i.pop('Pos')
     Tiles.append(i)
 
-print("getting data values...")
-#Get data into format
-Data_new = Zeroes * 256
-i = 0
-for a in range(0, 16):
-    for b in range(0, 16):
-        for z in range(a*16, a*16+16):
-            for x in range(b*16, b*16+16):
-
-                for j in range(0, 32):
-                    Data_new[i+j] = 0
-                i += 32
-                
-                for y in range(0, 64):
-                    Data_new[i] = (int(Data[(y * 256 + x) * 256 + z]) // 16) % 16
-                    i += 1
-
-                for j in range(0, 32):
-                    Data_new[i+j] = 0
-                i += 32
-
-
-print("converting data values...")
-Data_nibbles = []
-for i in range(0, 8388608, 2):
-                                    
-    byte1 = Data_new[i] * 16 + Data_new[i]
-    byte1 -= 256 * (byte1 > 127)
-    byte1 = Byte(byte1)                        
-                                    
-    Data_nibbles.append(byte1)
-
-    
-print("getting block values...")
+print("getting block ID data...")
 #Get blocks into format
 Blocks_new = Zeroes * 256
 i = 0
@@ -111,36 +94,80 @@ for a in range(0, 16):
                     Blocks_new[i+j] = Byte(0)
                 i += 32
 
+print("getting block DV data...")
+#Get data into format
+Data_new = Zeroes * 256
+i = 0
+for a in range(0, 16):
+    for b in range(0, 16):
+        for z in range(a*16, a*16+16):
+            for x in range(b*16, b*16+16):
+
+                for j in range(0, 32):
+                    Data_new[i+j] = 0
+                i += 32
+                
+                for y in range(0, 64):
+                    if Blocks_new[i] in [Byte(6), Byte(8), Byte(9), Byte(10), Byte(11), Byte(50), Byte(51), Byte(55), Byte(59), Byte(60), Byte(61), Byte(62)]:
+                        Data_new[i] = (int(Data[(y * 256 + x) * 256 + z]) // 16) % 16
+                    else:
+                        Data_new[i] = Byte(0)
+                    i += 1
+
+                for j in range(0, 32):
+                    Data_new[i+j] = 0
+                i += 32
+
+print("converting data...")
+Data_nibbles = []
+for i in range(0, 8388608, 2):
+                                    
+    byte1 = Data_new[i] * 16 + Data_new[i]
+    byte1 -= 256 * (byte1 > 127)
+    byte1 = Byte(byte1)                        
+                                    
+    Data_nibbles.append(byte1)
+
 print("calculating height map...")
 #Calculate height map
 HeightMap = []
-for i in range(0, 8388608, 128):
-    blocks_height_test = Blocks_new[i:i+128]
-    for j in range(127, 0, -1):
-        if blocks_height_test[j] != Byte(0):
-            HeightMap.append(Byte(j))
-            break
+for x in range(0, 256):
+    for i in range(0, 16):
+        for j in range(0, 16):
+
+            n = x*32768 + j*128*16 + i*128
+            blocks_height_test = Blocks_new[n:n+128]
+
+            for j in range(127, 0, -1):
+                if blocks_height_test[j] != Byte(0):
+                    HeightMap.append(Byte(j))
+                    break
                 
-print("making level.dat...")
+print("creating level.dat...")
 #make new level.dat
 new_file = File({
     '': Compound({
         'Data': Compound({
             'Player': Compound({
                 "Inventory": Inventory,
-                "Motion": List[Double]([Double(Motion0), Double(Motion1), Double(Motion2)]),
-                "Pos": List[Double]([Double(Pos0), Double(Pos1), Double(Pos2)]),
+                "Motion": List[Double]([Motion0, Motion1, Motion2]),
+                "Pos": List[Double]([Pos0, Pos1, Pos2]),
                 "Rotation": Rotation,
                 "Air": Air,
+                "AttackTime": AttackTime,
+                "DeathTime": DeathTime,
                 "FallDistance": FallDistance,
                 "Fire": Fire,
-                "id": id,
-                "Score": Score
+                "Health": Health,
+                "HurtTime": HurtTime,
+                "OnGround": OnGround,
+                "Score": Score,
                 }),
             'LastPlayed': LastPlayed,
-            'SpawnX': Int(SpawnX),
-            'SpawnY': Int(SpawnY),
-            'SpawnZ': Int(SpawnZ),
+            'SpawnX': SpawnX,
+            'SpawnY': SpawnY,
+            'SpawnZ': SpawnZ,
+            'Time': Time,
             })
         })
     })
@@ -148,7 +175,7 @@ new_file.save('World1/level.dat', gzipped=True)
 
 n = 0
 
-print("making chunk files...")
+print("creating chunk files...")
 #make c.*.*.dat
 for i in range(0, 16):
     for j in range(0, 16):
@@ -160,6 +187,7 @@ for i in range(0, 16):
                 tiles_new.append(k)
         
         n = 16*i + j
+        
         new_file = File({
             '': Compound({
                 'Level': Compound({
@@ -171,7 +199,7 @@ for i in range(0, 16):
                     'Data': ByteArray(Data_nibbles[n*16384:n*16384+16384]),
                     'BlockLight': ByteArray(Zeroes[0:16384]),
                     'SkyLight': ByteArray(Zeroes[0:16384]),
-                    'HeightMap': ByteArray(HeightMap[0:256])
+                    'HeightMap': ByteArray(HeightMap[n*256:n*256+256])
                     })
                 })
             })
